eb904b5da712c6ba5fea29bf5ab07de5546b88ff
weichengzhou <t105598056@ntut.org.tw>
2021-01-13 17:43:54 +0800

    Update EDMSAdminSite text

--- core/admin/base.py
+++ core/admin/base.py
@@ -3,8 +3,8 @@ from django.contrib.admin import AdminSite
 from django.utils.translation import ugettext_lazy as _
 
 class EDMSAdminSite(AdminSite):
-    site_title = _('EDMS 文件管理系統')
+    site_title = _('EDMS')
     site_header = _('EDMS 文件管理系統')
-    index_title = _('EDMS 管理介面')
+    index_title = _('管理介面')
 
 edms_admin = EDMSAdminSite()
